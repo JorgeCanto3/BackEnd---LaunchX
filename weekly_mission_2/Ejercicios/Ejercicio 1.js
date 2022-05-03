d7d15787064c60808d897affb2000be1eab37f78
JorgeCanto3 <launchx09367@innovaccion.mx>
2022-05-02 21:44:52 -0500

    Twitter Exercise, terminado

--- weekly_mission_2/Ejercicios/Ejercicio 1.js
+++ weekly_mission_2/Ejercicios/Ejercicio 1.js
@@ -1,4 +1,5 @@
 //Twitter
+
 const user = {
     Name_of_user: "Jorge Canto",
     UserName: "JorgeCanto2002",
@@ -7,34 +8,51 @@ const user = {
     Following: 71,
     User_Join: "User join June of 2017",
     RT: 894,
+    likes: 89,
     NumberTweets: 99,
     Location: "Mexico",
-} 
-const HashTag = {
     Name: "#Hashtag Name",
-    Time: "12:00"
-}
-const tendrigtopic = {
+    Time: "12:00",
     PrinipalTopic: "Evento:",
     ThemTopic: "Tendencia de 'Evento'",
-    tabsTredening: {
-        ForU: "Para ti",
-        Trends: "Tendencias",
-        News: "Noticias",
-        Sports: "Deportes"
+    tabsTredening: "Sin texto",
+    ForU: "Para ti",
+    Trends: "Tendencias",
+    News: "Noticias",
+    Sports: "Deportes",
+    Trend1: "Primer Trend",
+    HashTag1: "#Trend1",
+    NumTweets1: "##### de Tweets",
+    Trend2: "Segundo Trend",
+    HashTag2: "#Trend2",
+    NumTweets2: "##### de Tweets",
+    Trend3: "Tercer Trend",
+    HashTag3: "#Trend3",
+    NumTweets3: "##### de Tweets",
+    Trend4: "Cuarto Trend",
+    HashTag4: "#Trend4",
+    NumTweets4: "##### de Tweets",
+
+
+    getuser: function () {
+        return `perfil de ${this.Name_of_user}`
     },
-    Trends: {
-        Trend1: "Primer Trend",
-        HashTag1: "#Trend1",
-        NumTweets1:"##### de Tweets",
-        Trend2: "Segundo Trend",
-        HashTag2: "#Trend2",
-        NumTweets2: "##### de Tweets",
-        Trend3: "Tercer Trend",
-        HashTag3: "#Trend3",
-        NumTweets3: "##### de Tweets",
-        Trend4: "Cuarto Trend",
-        HashTag4: "#Trend4",
-        NumTweets4: "##### de Tweets",
-    } 
+    getTrendsandHAshtags: function () {
+        return `Trending:  ${this.ForU} | Hastag: ${this.HashTag1}`
+    },
+
+    getLikesandRt: function () {
+        return `Likes: ${this.likes} Rt: ${this.RT}`
+    },
+    getLocation: function () {
+        return `Es de: ${this.Location} `
+    }
 }
+
+console.log("Estas en Twitter en : " + user.getuser());
+console.log("Tendencias actuales:  " + user.getTrendsandHAshtags());
+console.log(user.getLikesandRt());
+console.log(user.getLocation());
+
+
+//Facebook
