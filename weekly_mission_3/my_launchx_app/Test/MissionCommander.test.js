a2106361be77eeae6485c61d42473866e49b623f
JorgeCanto3 <launchx09367@innovaccion.mx>
2022-05-04 22:21:09 -0500

    Test Run

--- weekly_mission_3/my_launchx_app/Test/MissionCommander.test.js
+++ weekly_mission_3/my_launchx_app/Test/MissionCommander.test.js
@@ -1,9 +1,9 @@
 const MissionCommander = require('./../App/MissionCommander')
 
 
-    describe("MissionCommander Test + Class", () => {
+describe("MissionCommander Test + Class", () => {
     test('1) Create a mission commander objet', () => {
-        const result = 1 + 2
-        expect(result).toBe(3);
+        const commanderTest = new MissionCommander("Que pasa nena")
+        expect(commanderTest.name).toBe("Que pasa nena");
     });
 })
