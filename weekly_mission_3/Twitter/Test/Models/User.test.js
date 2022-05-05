7724913fd5fb059f734148a97a8a4f0cbd126c97
JorgeCanto3 <launchx09367@innovaccion.mx>
2022-05-05 00:20:51 -0500

    Date and Last Changed

--- weekly_mission_3/Twitter/Test/Models/User.test.js
+++ weekly_mission_3/Twitter/Test/Models/User.test.js
@@ -3,7 +3,7 @@ const User = require('./../../App/Models/User')
 describe("Unit Test for User class", () => {
     test('Create an user objetc', () => {
         //Llamamos el codiog para la app
-        const usert = new User("1", "JorgeCanto", "Jorge", "Bio", "20 - 02 - 2022", "05-05-2022")
+        const usert = new User("1", "JorgeCanto", "Jorge", "Bio", "dateCreated", "lasUpdated")
         
         //Validamos el codigo
         //Comparacion que va a igualar el valor construido con el esperado
@@ -11,7 +11,7 @@ describe("Unit Test for User class", () => {
         expect(usert.username).toBe("JorgeCanto")
         expect(usert.name).toBe("Jorge")
         expect(usert.bio).toBe("Bio")
-        expect(usert.dateCreated).toBe("20 - 02 - 2022")
-        expect(usert.lasUpdated).toBe("05-05-2022")
+        expect(usert.dateCreated).toBe("dateCreated")
+        expect(usert.lasUpdated).toBe("lasUpdated")
     })
 })
