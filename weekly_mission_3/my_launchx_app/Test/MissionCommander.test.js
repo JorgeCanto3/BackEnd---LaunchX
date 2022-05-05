94702be212e49e27986284b6c6d0a2cd76a84a4e
JorgeCanto3 <launchx09367@innovaccion.mx>
2022-05-04 22:21:29 -0500

    Test Failed

--- weekly_mission_3/my_launchx_app/Test/MissionCommander.test.js
+++ weekly_mission_3/my_launchx_app/Test/MissionCommander.test.js
@@ -4,6 +4,6 @@ const MissionCommander = require('./../App/MissionCommander')
 describe("MissionCommander Test + Class", () => {
     test('1) Create a mission commander objet', () => {
         const commanderTest = new MissionCommander("Que pasa nena")
-        expect(commanderTest.name).toBe("Que pasa nena");
+        expect(commanderTest).toBe("Que pasa nena");
     });
 })
