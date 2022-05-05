fcdc01eae06aea2d4d47e35f6314646636132501
JorgeCanto3 <launchx09367@innovaccion.mx>
2022-05-04 22:59:19 -0500

    Tip to develop Test

--- weekly_mission_3/Spiderverse/Test/Spierman.test.js
+++ weekly_mission_3/Spiderverse/Test/Spierman.test.js
@@ -4,3 +4,11 @@ describe("Dummy D", () => {
         expect(ResultR).toBe(10);
     });
 })
+
+/*
+TDD Design Software Technique
+
+1. Write a test for the next bit of functionality you want to add.
+2. Write the functional code until the test passes.
+3. Refactor both new and old code to make it well structured.
+*/
