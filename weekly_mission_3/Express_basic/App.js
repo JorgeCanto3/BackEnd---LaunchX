8f77c3df943bbdfe33d4621e47ddc3cc6ad4450d
JorgeCanto3 <launchx09367@innovaccion.mx>
2022-05-05 20:36:29 -0500

    Port tested and working

--- weekly_mission_3/Express_basic/App.js
+++ weekly_mission_3/Express_basic/App.js
@@ -7,4 +7,12 @@ const app = express()
 //Puerto en que vamos a ver nuestra app: Localhost:3000
 const port = 3000
 
-//
+//path inicial, responderá a la url localhost:3000
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+//Inicializamos esta app de la siguiente manera
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
