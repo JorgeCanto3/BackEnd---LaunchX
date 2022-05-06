224f3c9c5057c615f6984634e6f5ad170542cbf5
JorgeCanto3 <launchx09367@innovaccion.mx>
2022-05-05 20:56:36 -0500

    Practica Realizada

--- weekly_mission_3/Express_basic/App.js
+++ weekly_mission_3/Express_basic/App.js
@@ -16,3 +16,24 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+//New rute
+//Answering Text
+//local.host:3000/launchx
+app.get('/Launchx', (req, res) => {
+    res.send('Bienvenido a casa manolo')
+})
+
+//Ruta para regresar un objeto
+//localhost:3000/explorersInNode
+app.get('/explorersInNode', (req, res) => {
+    const explorer = { name: "Explorer", msg: "Hey!" }
+    res.send(explorer)
+})
+
+//Query Params (Parámetros): Recibir parametros por la Url
+//http://localhost:3000/explorers/Jorge
+//rq.params = {"/explorerName":"Jorge"}
+app.get('/explorers/:explorerName', (req, res) => {
+    res.send(req.params)
+})
